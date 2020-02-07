05411174f0d49ab458a407bc9a6e4ba1761f9295
ZHANGQIANYI <zhangqianyi@deepwise.com>
2020-02-07 16:17:59 +0800

    fix point

--- zqy_utils/point.py
+++ zqy_utils/point.py
@@ -1,26 +1,34 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+import itertools
+__all__ = ["flat_nested_list", "get_bounding_box"]
 
-__all__ = ["get_bounding_box"]
+
+def flat_nested_list(nested_list):
+    return list(itertools.chain(*nested_list))
 
 
-def get_bounding_box(edge_list):
+def get_bounding_box(edge_list, dim=2):
     """
-    given a list of points, return its bounding box
+    given a (nested) list of points, return its bounding box
     args:
-        edge_list: {[[x,y],...]}
+        edge_list (list[points])
+        dim (int): point dimension, default is 2
         if the list is empty, all values are defaulted to -1
     return:
-        bounding_box: {2d np.array} (top_left, bot_right)
+        bounding_box: (2d np.array with shape 2xdim): [top_left, bot_right]
     """
-    edge_list_np = np.array(edge_list)
+    edge_list_np = np.array(flat_nested_list(edge_list))
     try:
-        top_left = edge_list_np.min(axis=0)
-        bot_right = edge_list_np.max(axis=0)
+        edge_list_np = edge_list_np.reshape(-1, dim)
+        min_point = edge_list_np.min(axis=0)
+        max_point = edge_list_np.max(axis=0)
+        bounding_box = np.vstack((min_point, max_point))
     except Exception:
         print(edge_list)
-        print(edge_list_np.dtype)
-        top_left = (-1, -1)
-        bot_right = (-1, -1)
-    return np.vstack((top_left, bot_right))
+        dtype = edge_list_np.dtype
+        print(dtype)
+        bounding_box = np.ones((2, dim), dtype=dtype) * -1
+    return bounding_box
+
