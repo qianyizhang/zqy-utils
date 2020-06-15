e5bf169ccf6f1dc2742d33467262b495a93fdc26
ZHANGQIANYI <zhangqianyi@deepwise.com>
2020-06-15 09:39:53 +0000

    fix plot

--- zqy_utils/plot.py
+++ zqy_utils/plot.py
@@ -20,6 +20,48 @@ def get_colors(labels, palette=DEFAULT_PALETTE):
     return colors
 
 
+def get_color_bar(colors, size, is_vertical):
+    num = len(colors)
+    # get appropriate patch size for each cell
+    h, w = size
+    if is_vertical:
+        w *= num
+    else:
+        h *= num
+    hs = [h//num] * num
+    ws = [w//num] * num
+    # hanlde the residual
+    hs[0] += h - sum(hs)
+    ws[0] += w - sum(ws)
+
+    patch_list = []
+    for color, h, w in zip(colors, hs, ws):
+        if isinstance(colors, dict):
+            label = color
+            color = colors[label]
+            p = np.ones((h, w, 3))*color
+            p = cv2.putText(p, str(label), (w//4, h*3//4),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1)
+        else:
+            p = np.ones((h, w, 3))*color
+        patch_list.append(p)
+
+    if is_vertical:
+        patch = np.vstack(patch_list)
+    else:
+        patch = np.hstack(patch_list)
+    return patch.astype(np.uint8)
+
+
+def test_get_color_bar():
+    import boxx
+    colors = get_colors([1, 2, 3, 4, 5])
+    size = (40, 200)
+    is_vertical = False
+    img = get_color_bar(colors, size, is_vertical)
+    boxx.show(img)
+
+
 def img_to_uint8(img, img_max=None, img_min=None):
     img_float = img.astype(np.float32)
     if img_max is None:
@@ -30,26 +72,64 @@ def img_to_uint8(img, img_max=None, img_min=None):
     return img_float.clip(0, 255).astype(np.uint8)
 
 
-def overlay_bboxes(image, boxes, colors, line_thickness=5):
+def overlay_bboxes(image, boxes, labels=None, colors=None,
+                   line_thickness=5, colorbar=0):
     image = np.array(image)
     if image.ndim == 2:
         image = image[..., None]
     assert image.ndim == 3, f"unknown image shape {image.shape}"
     if image.shape[0] in (1, 3):
         # chw -> hwc
-        image = image.transpose(1, 2, 0)
+        image = image.transpose(1, 2, 0).copy()
     if image.shape[-1] == 1:
         image = image.repeat(3, -1)
+    # image should be h * w * 3 now
+    if colors is None:
+        if labels is None:
+            colors = [None] * len(boxes)
+        else:
+            assert len(labels) == len(boxes), "box label size mismatch"
+            colors = get_colors(labels)
 
-    # image should be h *w* 3 now
+    boxes = np.array(boxes)
+    if image.dtype != np.uint8:
+        image = img_to_uint8(image)
+
+    image = np.ascontiguousarray(image)
 
     for box, color in zip(boxes, colors):
-        box = box.round().astype(int)
-        top_left, bottom_right = box[:2].tolist(), box[2:].tolist()
-        color = map(int, color)
-        image = cv2.rectangle(image, tuple(top_left), tuple(bottom_right),
-                              tuple(color), line_thickness)
+        box = box.round()
+        if color is None:
+            color = (255, 0, 0)  # default is red
+        else:
+            color = tuple(map(int, color))
+        image = cv2.rectangle(image,
+                              (int(box[0]), int(box[1])),
+                              (int(box[2]), int(box[3])),
+                              color, line_thickness)
+
+    if colorbar > 0:
+        h, w = image.shape[:2]
+        if labels is not None:
+            colors = dict(zip(labels, colors))
+        if colorbar == 1:
+            bar = get_color_bar(colors, (h, 40), is_vertical=True)
+            image = np.hstack([image, bar])
+        else:
+            bar = get_color_bar(colors, (40, w), is_vertical=False)
+            image = np.vstack([image, bar])
+
     return image
+
+
+def test_overlay_bboxes():
+    import boxx
+    img = np.random.random((200, 200))
+    bbox = [[10, 10, 40, 40], [20, 100, 50, 150]]
+    labels = [1, 3]
+
+    img = overlay_bboxes(img, bbox, labels=labels, colorbar=2)
+    boxx.show(img)
 
 
 def get_img_rois(img,
@@ -59,7 +139,8 @@ def get_img_rois(img,
                  padding=100,
                  line_thickness=1,
                  font_size=0.5,
-                 color=(255, 255, 255)):
+                 color=(255, 255, 255),
+                 alpha=1.0):
     if not isinstance(img, np.ndarray):
         img = np.array(img)
     if len(img.shape) == 2:
@@ -85,7 +166,7 @@ def get_img_rois(img,
             mask = np.array(mask).squeeze()
             if mask.shape == img.shape[:2]:
                 mask = mask[y0:y1, x0:x1, None] * color
-                new_img = new_img + mask
+                new_img = (1.0-alpha) * new_img + alpha * mask
             else:
                 mask = cv2.resize(mask,
                                   (roi[2] + 1 - roi[0], roi[3] + 1 - roi[1]))
@@ -94,8 +175,9 @@ def get_img_rois(img,
                 ix0, ix1 = np.clip([mx0, mx1], 0, x1 - x0)
                 my0, my1 = roi[1] - y0, roi[3] + 1 - y0
                 iy0, iy1 = np.clip([my0, my1], 0, y1 - y0)
-                new_img[iy0:iy1, ix0:ix1] += mask[(iy0 - my0):(iy1 - my0),
-                                                  (ix0 - mx0):(ix1 - mx0)]
+                new_img[iy0:iy1, ix0:ix1] *= (1.0-alpha)
+                new_img[iy0:iy1, ix0:ix1] += alpha * \
+                    mask[(iy0 - my0):(iy1 - my0), (ix0 - mx0):(ix1 - mx0)]
             new_img = new_img.clip(img_info.min,
                                    img_info.max).astype(img.dtype)
 
